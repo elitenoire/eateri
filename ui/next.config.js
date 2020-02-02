773cf05721b8e12eb521456036922e1dd522bf9d
Eva Raymond <evathecoderr@outlook.com>
2020-02-02 18:10:36 +0100

    inline svgs + import svg urls

--- ui/next.config.js
+++ ui/next.config.js
@@ -1,29 +1,45 @@
+const path = require('path')
 const withCSS = require('@zeit/next-css')
 const withPlugins = require('next-compose-plugins')
 const withFonts = require('next-fonts')
 const withImages = require('next-images')
-const withSvgr = require('next-svgr')
+// const withSvgr = require('@inabagumi/next-svg')
 
-module.exports = withPlugins([withCSS, withFonts, withSvgr])
-// module.exports = withImages({
-//     webpack: config => {
-//         config.module.rules.push({
-//             test: /\.(svg)$/,
-//             use: [
-//                 {
-//                     loader: '@svgr/webpack',
-//                     options: {
-//                         icon: true,
-//                         titleProp: true,
-//                         svgoConfig: {
-//                             plugins: {
-//                                 removeViewBox: false,
-//                             },
-//                         },
-//                     },
-//                 },
-//             ],
-//         })
-//         return config
-//     },
-// })
+const svgrOptions = {
+    titleProp: true,
+    svgoConfig: {
+        plugins: [
+            {
+                cleanupIDs: false,
+            },
+            { prefixIds: false },
+            { prefixClassNames: false },
+        ],
+    },
+}
+const imgOptions = {
+    exclude: [path.resolve(__dirname, 'public/inlineSvg'), path.resolve(__dirname, 'node_modules/remixicon')],
+}
+
+const withSvgr = (nextConfig = {}) => ({
+    ...nextConfig,
+    webpack(config, options) {
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: [
+                {
+                    loader: '@svgr/webpack',
+                    options: nextConfig.svgrOptions,
+                },
+            ],
+        })
+
+        if (typeof nextConfig.webpack === 'function') {
+            return nextConfig.webpack(config, options)
+        }
+
+        return config
+    },
+})
+
+module.exports = withPlugins([withCSS, withFonts, [withImages, imgOptions], [withSvgr, { svgrOptions }]])
