17f7904c614700171b45079f848d2fb2fafe38bf
Eva Raymond <evathecoderr@outlook.com>
2021-02-03 11:12:44 +0100

    [UI] Custom 404

---
+++ ui/pages/404/style.js
@@ -0,0 +1,49 @@
+const styles = {
+    container: {
+        display: 'flex',
+        maxWidth: 'maxContainer',
+        flexDirection: ['column', null, null, null, 'row'],
+        py: [9, null, null, null, 13],
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    doodle: {
+        position: 'relative',
+        overflow: 'hidden',
+        maxWidth: ['20em', null, null, null, '20em'],
+        width: 'inherit',
+
+        ':before': {
+            position: 'absolute',
+            content: '""',
+            width: '100%',
+            height: '100%',
+            bg: 'accent.pale',
+            borderRadius: '50%',
+            top: '60%',
+            left: 0,
+            zIndex: -1,
+        },
+    },
+    content: {
+        mt: [7, null, null, null, 0],
+        ml: [null, null, null, null, 11],
+        textAlign: ['center', null, null, null, 'left'],
+        transition: 'color 0.3s ease',
+        a: {
+            textDecorationColor: t => t.colors.primary.base,
+            textDecorationStyle: 'wavy',
+            textDecorationLine: 'underline',
+            mx: 2,
+            cursor: 'pointer',
+            ':hover': {
+                color: 'highlight.base',
+            },
+        },
+    },
+    links: {
+        my: 4,
+    },
+}
+
+export default styles
