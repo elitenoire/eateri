303baea5795cef3e22e40e13f83d99a570a3fea0
Eva Raymond <evathecoderr@outlook.com>
2021-03-07 14:58:04 +0100

    [UI] use next-seo

---
+++ ui/next-seo.config.js
@@ -0,0 +1,20 @@
+export default {
+    titleTemplate: '%s | Eateri',
+    defaultTitle: 'Eateri | The Messi of Great Food',
+    description: 'Dine in to sample our tasty dishes, or have it delivered to you. Make table reservations for free.',
+    additionalMetaTags: [
+        {
+            name: 'viewport',
+            content: 'initial-scale=1.0, width=device-width',
+            key: 'viewport',
+        },
+        {
+            name: 'application-name',
+            content: 'Eateri',
+        },
+        {
+            httpEquiv: 'x-ua-compatible',
+            content: 'IE=edge',
+        },
+    ],
+}
