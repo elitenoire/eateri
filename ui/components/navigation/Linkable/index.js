c0d230c433d668a9898fdbeb1823f57509f97ae8
Eva Raymond <evathecoderr@outlook.com>
2021-06-21 11:49:42 +0100

    [UI] option to use plain link or next link

--- ui/components/navigation/Linkable/index.js
+++ ui/components/navigation/Linkable/index.js
@@ -4,15 +4,30 @@ import Link from 'next/link'
 
 import styles from './style'
 
-export default function Linkable({ hover, className, children, accessibilityText, ...props }) {
+export default function Linkable({
+    hyperlink,
+    hover,
+    className,
+    href,
+    children,
+    accessibilityText,
+    onClick,
+    ...props
+}) {
     return (
         <div sx={styles.wrapper} className={className} data-hoverable={hover ? '' : null}>
             {children}
-            <Link passHref {...props}>
-                <a sx={styles.link}>
+            {hyperlink ? (
+                <a sx={styles.link} href={href} onClick={onClick} {...props}>
                     <span className="visually-hidden">{accessibilityText}</span>
                 </a>
-            </Link>
+            ) : (
+                <Link href={href} passHref {...props}>
+                    <a sx={styles.link}>
+                        <span className="visually-hidden">{accessibilityText}</span>
+                    </a>
+                </Link>
+            )}
         </div>
     )
 }
