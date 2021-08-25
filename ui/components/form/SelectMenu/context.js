524b1b600b2120daa802b8638cd34864cfcdd995
Eva Raymond <evathecoderr@outlook.com>
2021-08-25 13:04:42 +0100

    [UI] Select component

---
+++ ui/components/form/SelectMenu/context.js
@@ -0,0 +1,68 @@
+import { createContext, useMemo, useState, useEffect } from 'react'
+
+import { useMenuState } from 'reakit/Menu'
+
+import { isString, isObject } from '~/lib/utils'
+
+function getObjectValue(obj) {
+    const keys = Object.keys(obj)
+    return obj[keys[keys.length - 1]]
+}
+
+function normalizer(name, value) {
+    if (Array.isArray(value) || isString(value)) {
+        return { [name]: value }
+    }
+    return value
+}
+
+function makeOptions({ value, multi, group }) {
+    return function () {
+        const noInitialValue = value == null // value === undefined
+        const isGroup = isObject(value) || (group && noInitialValue)
+        const isMulti =
+            Array.isArray(value) ||
+            (multi && noInitialValue) ||
+            (isObject(value) && Array.isArray(getObjectValue(value)))
+        return { isGroup, isMulti }
+    }
+}
+
+export const SelectContext = createContext()
+
+/**
+ *
+ * autoClose - applies only to single select
+ * autoSelect -> select on focus
+ * hasTags -> multi select options as tags
+ * searchable -> search box in the listbox
+ * collapsible -> group options are collapsible
+ * placeholder
+ * showCheckbox
+ * selectionLimit -> applies only to multi select
+ */
+
+function Select({ name = 'test-item', value, onChange, multi, group, children, ...initialState }) {
+    const menu = useMenuState({ unstable_values: normalizer(name, value), ...initialState })
+
+    const [{ isGroup, isMulti }] = useState(makeOptions({ value, multi, group }))
+    const context = { ...initialState, ...menu, isMulti, isGroup }
+    const values = useMemo(() => context, Object.values(context))
+
+    const _value = values.unstable_values
+
+    useEffect(() => {
+        let selected = _value
+
+        if (!isGroup) {
+            // const keys = Object.keys(_value)
+            // const latest = _value[keys[keys.length - 1]]
+            selected = getObjectValue(_value)
+        }
+
+        onChange?.(selected)
+    }, [_value, isGroup, onChange])
+    return <SelectContext.Provider value={values}>{children}</SelectContext.Provider>
+}
+
+export default Select
---
+++ ui/components/form/SelectMenu/index.js
@@ -0,0 +1,95 @@
+/** @jsx jsx */
+import { jsx } from '@theme-ui/core'
+import { forwardRef, useContext, Children, isValidElement, cloneElement } from 'react'
+import { MenuButton, Menu, MenuItemRadio, MenuItemCheckbox, MenuGroup } from 'reakit/Menu'
+import { unstable_IdProvider as IdProvider, unstable_useId as useId } from 'reakit/Id'
+
+import Dropdown from '../Dropdown'
+import Select, { SelectContext } from './context'
+
+export const SelectButton = forwardRef(function ({ children, ...rest }, ref) {
+    const select = useContext(SelectContext)
+
+    return (
+        <MenuButton
+            ref={ref}
+            {...select}
+            {...rest}
+            // value={value}
+            active={select.visible}
+            aria-haspopup="listbox"
+        >
+            {htmlProps => <Dropdown.Button {...htmlProps}>{children}</Dropdown.Button>}
+        </MenuButton>
+    )
+})
+
+export const SelectList = forwardRef(function ({ prefix, id, children, name, ...rest }, ref) {
+    const { isGroup, isMulti, hasTags, ...select } = useContext(SelectContext)
+    const asProp = isGroup ? 'div' : 'ul'
+    const showTags = isMulti && hasTags
+    const values = Object.values(select.unstable_values)
+
+    return (
+        <Menu ref={ref} {...select} {...rest} id={prefix || id}>
+            {htmlProps => (
+                <IdProvider prefix={`${htmlProps.id}-gp`}>
+                    <Dropdown.List {...htmlProps} as={asProp} role="listbox">
+                        {showTags && (
+                            <div>
+                                {values.map((array = [], i) =>
+                                    array.map((_value, j) => <span key={i + j}>{_value}</span>)
+                                )}
+                            </div>
+                        )}
+                        {Children.map(children, child =>
+                            !isGroup && name && isValidElement(child) ? cloneElement(child, { name }) : child
+                        )}
+                    </Dropdown.List>
+                </IdProvider>
+            )}
+        </Menu>
+    )
+})
+
+export const SelectGroup = forwardRef(function ({ id: defaultId, label, name, children, ...rest }, ref) {
+    const select = useContext(SelectContext)
+    const { id } = useId({ id: defaultId })
+
+    return (
+        <MenuGroup ref={ref} {...select} {...rest}>
+            {htmlProps => (
+                <ul {...htmlProps} aria-labelledby={id}>
+                    <li id={id} role="presentation">
+                        {label}
+                    </li>
+                    {Children.map(children, child =>
+                        name && isValidElement(child) ? cloneElement(child, { name }) : child
+                    )}
+                </ul>
+            )}
+        </MenuGroup>
+    )
+})
+
+export const SelectOption = forwardRef(function ({ children, ...rest }, ref) {
+    const { isMulti, ...select } = useContext(SelectContext)
+    const Element = isMulti ? MenuItemCheckbox : MenuItemRadio
+
+    return (
+        <Element ref={ref} {...select} {...rest}>
+            {({ 'aria-checked': ariaChecked, ...htmlProps }) => (
+                <Dropdown.Item as="li" {...htmlProps} role="option" aria-selected={ariaChecked}>
+                    {children}
+                </Dropdown.Item>
+            )}
+        </Element>
+    )
+})
+
+Select.Button = SelectButton
+Select.List = SelectList
+Select.Group = SelectGroup
+Select.Item = SelectOption
+
+export default Select
