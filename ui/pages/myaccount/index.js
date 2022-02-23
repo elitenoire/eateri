e9d548d3341c77093f486c14a645ac155e96dc0b
Eva Raymond <evathecoderr@outlook.com>
2022-02-23 07:53:19 +0100

    [UI] my account dashboard

--- ui/pages/myaccount/index.js
+++ ui/pages/myaccount/index.js
@@ -1,10 +1,238 @@
+import { Box, Flex, Badge, Link } from '@theme-ui/components'
 import { Text } from '~@core/typography'
+import { StackGroup } from '~@core/display'
+import { Avatar, Icon, Button } from '~@core/general'
+import { PayWith } from '~@core/other'
+import OverviewCard from '~@common/card/OverviewCard'
+import FoodCard from '~@common/card/FoodCard'
+
 import { getLayout as getAccountLayout } from '~@common/layout/AccountLayout'
+
+import styles from '~/styles/myaccount'
+import url from '~/public/dish.png'
 
 function MyAccountPage() {
     return (
         <div>
-            <Text>MY ACCOUNT</Text>
+            <OverviewCard icon="orders" title="Orders">
+                <div sx={styles.twoRow}>
+                    <div sx={styles.box}>
+                        <div sx={styles.info}>
+                            <Text size={1} weight="bold">
+                                Recent Order
+                            </Text>
+                            <Text size={0} mb={2} color="textFade">
+                                #00231
+                            </Text>
+                            <StackGroup mt={1} mb={2} total={8} max={2} size="2em">
+                                <Avatar name="BI" ring={false} />
+                                <Avatar name="CD" ring={false} />
+                                <Avatar name="EA" ring={false} />
+                            </StackGroup>
+                            <Text size={0} color="textFade">
+                                on{' '}
+                                <Text as="span" weight="bold">
+                                    02 Jan 2022
+                                </Text>
+                            </Text>
+                            <Badge as="p" variant="accent">
+                                pick up
+                            </Badge>
+                        </div>
+                        <div sx={styles.boxAction}>
+                            <Link variant="arrow:xs" color="grayDarker">
+                                View
+                            </Link>
+                        </div>
+                    </div>
+                    <div sx={styles.box} data-design data-primary>
+                        <div sx={styles.stats}>
+                            <Text size={6} weight="bold">
+                                8
+                            </Text>
+                            <Text size={0} mb={1} weight="bold">
+                                Orders
+                            </Text>
+                            <Text size={0} color="text">
+                                January, 2022
+                            </Text>
+                        </div>
+                    </div>
+                </div>
+            </OverviewCard>
+            <OverviewCard icon="reserved" title="Reservations">
+                <div sx={styles.twoRow}>
+                    <div sx={styles.box}>
+                        <div sx={styles.info}>
+                            <Text size={1} weight="bold">
+                                Recent Reservation
+                            </Text>
+                            <Text size={0} mb={2} color="textFade">
+                                #GHYUBV
+                            </Text>
+                            <Text size={0} color="textFade">
+                                for{' '}
+                                <Text as="span" weight="bold" color="accent.base">
+                                    Jay Burg
+                                </Text>
+                            </Text>
+                            <Text size={0} color="textFade">
+                                on{' '}
+                                <Text as="span" weight="bold">
+                                    16 Apr 2022
+                                </Text>{' '}
+                                at{' '}
+                                <Text as="span" weight="bold">
+                                    18:35 PM
+                                </Text>
+                            </Text>
+                            <Badge as="p" variant="primary">
+                                confirmed
+                            </Badge>
+                        </div>
+                        <div sx={styles.boxAction}>
+                            <Link variant="arrow:xs" color="grayDarker">
+                                View
+                            </Link>
+                        </div>
+                    </div>
+                    <div sx={styles.box} data-design data-accent>
+                        <div sx={styles.stats}>
+                            <Text size={6} weight="bold">
+                                0
+                            </Text>
+                            <Text size={0} mb={1} weight="bold">
+                                Reservations
+                            </Text>
+                            <Text size={0} color="text">
+                                January, 2022
+                            </Text>
+                        </div>
+                    </div>
+                </div>
+            </OverviewCard>
+
+            <OverviewCard icon="heart" title="My Wishlist">
+                <div sx={styles.box}>
+                    <div sx={styles.boxHeader}>
+                        <Text size={0} color="textFade">
+                            Latest products saved:
+                        </Text>
+                    </div>
+                    <div sx={styles.wishBox}>
+                        <div sx={styles.cardWrap}>
+                            <FoodCard imgUrl={url} rating="" title="Chicken Briyani soup" shadow="none" fluid onClick />
+                        </div>
+                        <div sx={styles.cardWrap}>
+                            <FoodCard
+                                imgUrl={url}
+                                rating=""
+                                title="Teriyaki Sauce & Rice"
+                                shadow="none"
+                                fluid
+                                onClick
+                            />
+                        </div>
+                    </div>
+                    <div sx={styles.wishBoxAction}>
+                        <Button brand="subtle" bg="gray" outlineColor="grayMedium" outline fluid>
+                            More
+                        </Button>
+
+                        {/* <Button size="md" shape="pill" brand="outline" color="secondary" fluid>
+                            More
+                        </Button> */}
+                    </div>
+                </div>
+            </OverviewCard>
+
+            <div sx={styles.twoColumn}>
+                <OverviewCard icon="profile" title="My Details">
+                    <div sx={styles.box}>
+                        <div sx={styles.info}>
+                            <Text size={0} color="primary.base" weight="bold">
+                                Miss
+                            </Text>
+                            <Text size={3} weight="bold" line="tight">
+                                Eva
+                            </Text>
+                            <Text size={3} weight="bold" line="tight" mb={2}>
+                                Raymond
+                            </Text>
+                            <Text size={0} mb={2} line="tight" sx={styles.flex}>
+                                <span sx={styles.iconWrap}>
+                                    <Icon name="cake" />
+                                </span>
+                                29th September 1973
+                            </Text>
+                            <Text size={0} line="tight" wordBreak sx={styles.flex}>
+                                <span sx={styles.iconWrap}>
+                                    <Icon name="mail" />
+                                </span>
+                                testmail1087@gmail.com
+                            </Text>
+                        </div>
+                        <div sx={styles.boxAction}>
+                            <Link variant="arrow:xs" color="grayDarker">
+                                View
+                            </Link>
+                        </div>
+                    </div>
+                </OverviewCard>
+                <OverviewCard icon="addressbook" title="Address Book">
+                    <div sx={styles.box}>
+                        <div sx={styles.info}>
+                            <Text size={0} color="primary.base" weight="bold">
+                                Home
+                            </Text>
+                            <Text size={3} weight="bold" line="tight" mb={2}>
+                                Nigeria
+                            </Text>
+                            <Text size={0} mb={2} line="tight" sx={styles.flex}>
+                                <span sx={styles.iconWrap}>
+                                    <Icon name="useralt" />
+                                </span>
+                                Miss. Eva Raymond
+                            </Text>
+                            <Text size={0} mb={2} line="tight" sx={styles.flex}>
+                                <span sx={styles.iconWrap}>
+                                    <Icon name="location" />
+                                </span>
+                                25A Upper Marshall Street, Postbox Apartments, Central District, Abuja, 900108
+                            </Text>
+                            <Text size={0} mb={2} line="tight" sx={styles.flex}>
+                                <span sx={styles.iconWrap}>
+                                    <Icon name="phone" />
+                                </span>
+                                07034587655
+                            </Text>
+                            <Badge as="p" variant="secondary">
+                                Default
+                            </Badge>
+                        </div>
+                        <div sx={styles.boxAction}>
+                            <Link variant="arrow:xs" color="grayDarker">
+                                View
+                            </Link>
+                        </div>
+                    </div>
+                </OverviewCard>
+            </div>
+            <OverviewCard icon="bankcard" title="Payments">
+                <div sx={styles.box}>
+                    <div sx={styles.payWrap}>
+                        <PayWith mr={1} method="stripe" />
+                        <Badge as="p" variant="secondary">
+                            Default
+                        </Badge>
+                    </div>
+                </div>
+                <div sx={styles.payAction}>
+                    <Text as="a" size={1} weight="bold" decoration="underline" color="grayDarker">
+                        Change
+                    </Text>
+                </div>
+            </OverviewCard>
         </div>
     )
 }
---
+++ ui/styles/myaccount.js
@@ -0,0 +1,115 @@
+const styles = {
+    box: {
+        position: 'relative',
+        flex: 'auto', // 1
+        display: 'flex',
+        flexWrap: 'wrap',
+        rowGap: 6,
+        columnGap: 4,
+        bg: 'white',
+        borderRadius: 'default',
+        p: 4,
+        overflow: 'hidden',
+        '&[data-primary]': {
+            bg: 'primary.pastel',
+            color: 'primary.pale',
+        },
+        '&[data-accent]': {
+            bg: 'accent.pastel',
+            color: 'accent.pale',
+        },
+        '&[data-design]:after': {
+            content: '""',
+            position: 'absolute',
+            width: '40%',
+            pb: '40%',
+            top: '-20%',
+            right: '-20%',
+            borderRadius: 'circle',
+        },
+        '&[data-design][data-primary]:after': {
+            boxShadow: ({ colors }) =>
+                `0 0 0 0.75em ${colors.secondary.hover}, 0 0 0 1.25em ${colors.primary.base}, 0 0 0 1.95em ${colors.grayLight}`,
+        },
+        '&[data-design][data-accent]:after': {
+            boxShadow: ({ colors }) =>
+                `0 0 0 0.75em ${colors.secondary.hover}, 0 0 0 1.25em ${colors.accent.base}, 0 0 0 1.95em ${colors.grayLight}`,
+        },
+    },
+    info: {
+        minWidth: '7.5em',
+    },
+    stats: {
+        minWidth: '5.5em',
+        alignSelf: 'center',
+        zIndex: 1,
+    },
+    boxAction: {
+        flex: '10%',
+        alignSelf: 'flex-end',
+        textAlign: 'right',
+    },
+    boxHeader: {
+        flex: '100%',
+    },
+    wishBox: {
+        flex: 1,
+        display: 'flex',
+        justifyContent: 'space-evenly',
+        columnGap: 4,
+    },
+    wishBoxAction: {
+        flex: 1,
+        alignSelf: 'center',
+    },
+    cardWrap: {
+        width: '10em',
+        p: 4,
+        borderTopLeftRadius: '40',
+        borderTopRightRadius: '40',
+        borderBottomLeftRadius: 'default',
+        borderBottomRightRadius: 'default',
+        border: t => `1px solid ${t.colors.grayLight}`,
+    },
+    twoRow: {
+        flex: 1,
+        display: 'flex',
+        flexDirection: ['column-reverse', null, 'row'],
+        gap: 5,
+    },
+    twoColumn: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        columnGap: 5,
+        '& > div': {
+            flex: ['100%', null, null, null, 1],
+        },
+    },
+    flex: {
+        display: 'flex',
+        alignItems: 'center',
+    },
+    iconWrap: {
+        p: 2,
+        borderRadius: 'tiny',
+        mr: 2,
+        bg: 'primary.light',
+    },
+    payWrap: {
+        flex: 1,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
+    payAction: {
+        flex: 1,
+        alignSelf: 'center',
+        pr: 2,
+        textAlign: 'right',
+        'a:hover': {
+            color: 'inherit',
+        },
+    },
+}
+
+export default styles
