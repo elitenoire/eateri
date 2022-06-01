35b5f50f5f1b9e4e86af160449dbdfe84fb6b6bd
Eva Raymond <evathecoderr@outlook.com>
2022-06-01 18:17:40 +0100

    [UI] support select on focus move

--- ui/components/core/form/SelectBox/index.js
+++ ui/components/core/form/SelectBox/index.js
@@ -1,4 +1,4 @@
-import { forwardRef, createContext, useContext, useCallback } from 'react'
+import { forwardRef, createContext, useContext, useCallback, useEffect } from 'react'
 import { useCompositeState, Composite, CompositeItem } from 'ariakit/Composite'
 import { Checkbox, useCheckboxState } from 'ariakit/Checkbox'
 import Scrollable from '~@core/display/Scrollable'
@@ -6,25 +6,47 @@ import Scrollable from '~@core/display/Scrollable'
 const SelectBoxContext = createContext()
 
 export const SelectBox = forwardRef(function SelectBox(
-    { value, onChange, unselect, loop = true, children, compositeState, ...rest },
+    { value, onChange, unselect, loop = true, virtual, focusSelect, children, compositeState, ...rest },
     ref
 ) {
     const composite = useCompositeState({
         ...compositeState,
+        virtualFocus: virtual,
         focusLoop: loop,
     })
 
     const checkbox = useCheckboxState({ value, setValue: onChange })
 
-    const ariaProps = {
-        role: 'listbox',
-        ...(Array.isArray(checkbox.value) && { 'aria-multiselectable': true }),
-    }
-
+    const { activeId, items, moves, setMoves, virtualFocus } = composite
+    const { setValue } = checkbox
     const contextValue = { ...composite, ...checkbox, unselect }
 
+    const multiSelectable = Array.isArray(checkbox.value)
+    const selectOnMove = (virtualFocus || focusSelect) && !multiSelectable
+
+    const ariaProps = {
+        role: 'listbox',
+        ...(multiSelectable && { 'aria-multiselectable': true }),
+    }
+
+    // Reset moves to allow select onClick
+    const handleMouse = useCallback(() => {
+        if (!selectOnMove) return
+        setMoves(0)
+    }, [selectOnMove, setMoves])
+
+    // Select option on focus / move
+    useEffect(() => {
+        if (!selectOnMove || !moves) return
+
+        const selected = items.find(item => item.id === activeId)
+        if (!selected) return
+
+        setValue(selected.value)
+    }, [selectOnMove, activeId, items, moves, setValue])
+
     return (
-        <Composite state={composite} {...rest} {...ariaProps} ref={ref}>
+        <Composite state={composite} {...rest} {...ariaProps} onMouseDown={handleMouse} ref={ref}>
             {compositeProps => (
                 <Scrollable as="ul" {...compositeProps}>
                     <SelectBoxContext.Provider value={contextValue}>{children}</SelectBoxContext.Provider>
@@ -34,7 +56,7 @@ export const SelectBox = forwardRef(function SelectBox(
     )
 })
 
-export const SelectBoxOption = forwardRef(function SelectBoxOption({ value, children, ...rest }, ref) {
+export const SelectBoxOption = forwardRef(function SelectBoxOption({ value, disabled, children, ...rest }, ref) {
     const { unselect, setValue: _setValue, ...context } = useContext(SelectBoxContext)
 
     const setValue = useCallback(
@@ -49,13 +71,23 @@ export const SelectBoxOption = forwardRef(function SelectBoxOption({ value, chil
         [_setValue, unselect, value]
     )
 
+    const getItem = useCallback(item => ({ ...item, value: disabled ? undefined : value }), [disabled, value])
+
     // eslint-disable-next-line react-perf/jsx-no-new-object-as-prop
     const checkbox = { ...context, setValue }
 
     return (
-        <Checkbox as={CompositeItem} value={value} state={checkbox} {...rest} ref={ref}>
+        <Checkbox
+            as={CompositeItem}
+            value={value}
+            state={checkbox}
+            getItem={getItem}
+            disabled={disabled}
+            {...rest}
+            ref={ref}
+        >
             {({ 'aria-checked': ariaChecked, ...checkboxProps }) => (
-                <li {...checkboxProps} role="option" aria-selected={ariaChecked}>
+                <li {...checkboxProps} aria-selected={ariaChecked} role="option">
                     {children}
                 </li>
             )}
